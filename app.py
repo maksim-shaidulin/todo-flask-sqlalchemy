b6d5e521b806c29ecd0df8fd3982882a53556931
Maksim Shaydulin <mshaydulin@gmail.com>
2020-06-14 19:07:45 +0200

    add database.py to hangle DB interaction

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import Flask, request, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from . import create_app
+from . import create_app, database
 from .models import db, Todo
 
 app = create_app()
@@ -9,32 +9,31 @@ app = create_app()
 
 @app.route("/")
 def index():
-    todo_list = Todo.query.all()
+    todo_list = database.get_all(Todo, Todo.created_at)
     return render_template("index.html", todo_list=todo_list)
 
 
-@app.route("/add", methods=["Post"])
+@app.route("/add", methods=["POST"])
 def add():
-    text = request.form["text"]
-    db.session.add(Todo(text))
-    db.session.commit()
-    flash("New todo is added")
+    if "text" in request.form:
+        text = request.form["text"]
+        database.add_instance(Todo, text=text)
+        flash("New todo is added")
+    else:
+        flash("Error: no 'text' in data!")
     return redirect(url_for("index"))
 
 
 @app.route("/delete/<int:id>", methods=["POST"])
 def delete(id):
-    item = Todo.query.get(id)
-    db.session.delete(item)
-    db.session.commit()
+    database.delete_instance(Todo, id)
     flash(f"Item {id} is deleted")
     return redirect(url_for("index"))
 
 
 @app.route("/toggle_done/<int:id>", methods=["POST"])
 def toggle_done(id):
-    item = Todo.query.get(id)
-    item.done = not item.done
-    db.session.add(item)
-    db.session.commit()
+    todo = database.get_instance(Todo, id)
+    todo.done = not todo.done
+    database.edit_instance(Todo, id)
     return redirect(url_for("index"))
---
+++ database.py
@@ -0,0 +1,36 @@
+from .models import db
+
+
+def get_all(model, order=None):
+    if order is not None:
+        data = model.query.order_by(order)
+    else:
+        data = model.query.all()
+    return data
+
+
+def get_instance(model, id):
+    instance = model.query.filter_by(id=id)[0]
+    return instance
+
+
+def add_instance(model, **kwargs):
+    instance = model(**kwargs)
+    db.session.add(instance)
+    commit_changes()
+
+
+def delete_instance(model, id):
+    model.query.filter_by(id=id).delete()
+    commit_changes()
+
+
+def edit_instance(model, id, **kwargs):
+    instance = model.query.filter_by(id=id).all()[0]
+    for attr, new_value in kwargs:
+        setattr(instance, attr, new_value)
+    commit_changes()
+
+
+def commit_changes():
+    db.session.commit()
